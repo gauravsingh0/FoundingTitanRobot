685f32d2aeb92eda13f862567e85140a5fd46c61
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-06-26 17:52:46 +0530

    Update __init__.py

--- SaitamaRobot/modules/sql/__init__.py
+++ SaitamaRobot/modules/sql/__init__.py
@@ -1,24 +1,15 @@
-from SaitamaRobot import DB_URI, LOGGER
-
+from SaitamaRobot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 def start() -> scoped_session:
     engine = create_engine(DB_URI, client_encoding="utf8")
-    log.info("[PostgreSQL] Connecting to ElephantSQL database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
 BASE = declarative_base()
-try:
-    SESSION = start()
-except Exception as e:
-    log.exception(f'[PostgreSQL] Failed to connect to the database due to {e}')
-    exit()
-   
-log.info("[PostgreSQL] Connection to ElephantSQL successful, session started.")
+SESSION = start()
